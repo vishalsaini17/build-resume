093de1a57f79d5eeef0d859c22808da987dbe28b
vishalsaini17 <vsaini17@hotmail.com>
2024-02-06 15:51:18 +0530

    update print view size

--- src/App/layouts/ResumeLayout/index.tsx
+++ src/App/layouts/ResumeLayout/index.tsx
@@ -1,5 +1,5 @@
 import PrintRoundedIcon from '@mui/icons-material/PrintRounded';
-import { Container, Fab } from "@mui/material";
+import { Fab } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import PrintBlock from "../../component/atoms/PrintBlock";
 
@@ -11,11 +11,9 @@ const ResumeLayout: React.FC<props> = () => {
   return (
     <>
       <h3>header content</h3>
-      <Container>
-        <PrintBlock>
-          <Outlet />
-        </PrintBlock>
-      </Container>
+      <PrintBlock>
+        <Outlet />
+      </PrintBlock>
       <div className={styles.printBtn}>
         <Fab variant="extended" color="primary" onClick={() => window.print()}>
           <PrintRoundedIcon sx={{ mr: 1 }} />
